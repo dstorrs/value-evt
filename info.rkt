9df57725563000bd94afce7ff918c1e4f7205906
dstorrs <dstorrs.public@gmail.com>
2021-04-01 11:45:13 -0400

    updated description in info.rkt

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "value-evt")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "sandbox-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/value-evt.scrbl" ())))
-(define pkg-desc "Wraps an arbitrary value into a synchronizable event.  The synchronization result of the event is the original value.")
+(define pkg-desc "Allows syncing on arbitrary values.  Events are unchanged, procedures sync to their return value unless told not to, lists sync recursively, all other values sync to themselves.")
 (define version "0.1")
 (define pkg-authors '("David K. Storrs"))
